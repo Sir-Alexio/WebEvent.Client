a1ee38240b5cd2e3891b091aac46fe3845b03403
Alexey <lmokhareva@gmail.com>
2023-11-01 21:55:52 +0300

    Add connection to the server

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+using WebEvent.API.Model.DTO;
+using WebEvent.API.Model.ErrorModel;
+
+namespace WebEvent.Client.Controllers
+{
+    [Route("registration")]
+    public class RegistrationController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public RegistrationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("Client");
+        }
+
+        [Route("registration-page")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("registrate")]
+        public async Task<ActionResult> OnPost(UserDto dto)
+        {
+            JsonContent content = JsonContent.Create(dto);
+
+            using HttpResponseMessage response = await _httpClient.PostAsync("api/account/registration", content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                CustomException? errorMessage = JsonSerializer.Deserialize<CustomException>(await response.Content.ReadAsStringAsync());
+
+                ViewBag.Message = errorMessage.Message;
+
+                return View("Index", dto);
+            }
+
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient("Client", c => c.BaseAddress = new System.Uri("https://localhost:7141/"));
 
 var app = builder.Build();
 
